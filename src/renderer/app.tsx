9d15bababcb410df0270c6fd965cdd51b79258f1
evgeniy.kryazhev <evgeniy.kryazhev@actonica.ru>
2022-08-15 12:09:16 +0700

    change structure project

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './app.css';
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  ReactDOM.render(
+    <div>Hello world</div>, document.body);
 }
 
 render();
